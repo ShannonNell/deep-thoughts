c9ec2bad97dce5afbf9f933c48ca44d9ab552058
Shannon <shannon.nell92@gmail.com>
2021-08-30 10:55:02 -0400

    Create thought type definition and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,12 @@
+const { User, Thought } = require('../models');
+
 // resolver serves as the response for the helloWorld query in typeDefs.js
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async (parent, { username }) => {
+            // ? checks if username exists; if yes, set params to an object with username key set to the vlaue, otherwise return empty object
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
         }
     }
 };
