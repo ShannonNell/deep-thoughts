fb1cca4f8f04fdb6bd11f579da158ca7eeb06afd
Shannon <shannon.nell92@gmail.com>
2021-09-01 11:31:12 -0400

    Add friend functionality

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021 by Shannon Nell</div>
+      <div className="container">
+        &copy;{new Date().getFullYear()} by Shannon Nell
+      </div>
     </footer>
   );
 };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -24,9 +24,8 @@ const Login = (props) => {
 
     try {
       const { data } = await login({
-        variables: { ...formState }
+        variables: { ...formState },
       });
-      console.log(data);
 
       Auth.login(data.login.token);
     } catch (e) {
@@ -41,34 +40,35 @@ const Login = (props) => {
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card">
+          <h4 className="card-header">Login</h4>
+          <div className="card-body">
             <form onSubmit={handleFormSubmit}>
               <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
                 value={formState.email}
                 onChange={handleChange}
               />
               <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
+              <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
